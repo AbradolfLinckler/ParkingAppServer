90a18ddedf2d22ff61d803be33505dc9b2a1afcf
AbradolfLinckler <gaurav19091@gmail.com>
2021-11-28 07:28:04 +0530

    gg

---
+++ src/main/java/App/Server/ParkingSpace.java
@@ -0,0 +1,19 @@
+package App.Server;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParkingSpace {
+  @Id
+  private String id;
+  
+
+}
---
+++ src/main/java/App/Server/ParkingSpaceRepo.java
@@ -0,0 +1,5 @@
+package App.Server;
+
+public class ParkingSpaceRepo {
+  
+}
